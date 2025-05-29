4b859862906528f499d82a8a87c348e429fa3994
yps22 <yps22pd9988@gmail.com>
2025-05-29 15:42:02 +0900

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: [
+    "http://localhost:3001",
+    "http://192.168.0.15:3001"
+  ]
+};
+
+module.exports = nextConfig;
