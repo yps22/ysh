1b88e578e0ad2ca2f1d0f0431074a273b5189d69
yps22 <123833986+yps22@users.noreply.github.com>
2025-08-02 07:11:27 +0900

    Revert "Add lawyer photo to homepage"
    This reverts commit 84cc4faca77006511a36907824d29165fc6c3efc.

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,11 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          src="/untitled1.jpg"
-          alt="윤승환 변호사"
-          width={500}
-          height={500}
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
           priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
